f538e8b81de56ed547c7c3d9012a37b62e65a968
Madhugamidi14 <madhugamidi14021996@gmail.com>
2022-11-27 17:52:23 +0530

    changes applied

--- git_commands.py
+++ git_commands.py
@@ -8,7 +8,7 @@ git add . -- before commiting changes this command needs to be run to move the c
 git commit -m "your message"  -- run to commit your changes
 git remote add origin <your repo link> -- this will allow to connect your repo from your local machine
 git push origin main -- this pushes your changes to the repo
-git branch -M main -- changes the name of the branch to main or master etc
+git branch -M main -- changes the name of the branch to main or master etc.
 
 
 """
