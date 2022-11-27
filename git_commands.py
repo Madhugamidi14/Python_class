4f23374ce662cbea07555aad23fe3bd4cec0608f
Madhugamidi14 <madhugamidi14021996@gmail.com>
2022-11-27 16:13:03 +0530

    changes made by Madhugamidi

--- git_commands.py
+++ git_commands.py
@@ -1,1 +1,14 @@
-print("Hello world")
+""" 
+
+This sample file is created to demonstrate
+git push/pull requests 
+
+git init
+git add .
+git commit -m "your message"
+git remote add origin <your repo link>
+git push origin main
+git branch -M main
+
+
+"""
