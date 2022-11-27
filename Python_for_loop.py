84d0918cda3dbc0187d12f20f416aea3ad46fa7d
Madhugamidi14 <117514541+Madhugamidi14@users.noreply.github.com>
2022-11-27 10:27:25 +0530

    Create Python_for_loop.py

---
+++ Python_for_loop.py
@@ -0,0 +1,24 @@
+#write a code to print number from 1 to 10
+
+for num in range(1,11):
+    print(num)
+
+#write a code to print number from 1 to 10 using 2 steps
+
+for num in range(1,11,2):
+    print(num)
+
+#write a program to print numbers from 10 to 1
+
+for num in range(10,0,-1):
+    print(num)
+
+
+int_list = [4,8,-2,10,5]
+sum=0
+
+for i in int_list:
+    sum= sum+i
+print("Total sum of the integers in the list is:",sum)
+
+
