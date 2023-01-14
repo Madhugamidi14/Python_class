8bab4111f444c0c9d64165c97050a062be414ae9
Madhugamidi14 <madhugamidi14021996@gmail.com>
2023-01-14 15:25:13 +0530

    Success

--- git_commands.py
+++ git_commands.py
@@ -8,6 +8,6 @@ git commit -m "your message"  -- run to commit your changes
 git remote add origin <your repo link> -- this will allow to connect your repo from your local machine
 git push origin main -- this pushes your changes to the repo
 git branch -M main -- changes the name of the branch to main or master etc.
-
+git pull origin main -- this needs to be used whenever we start working so as to get the updated code from the shared repository
 
 """
